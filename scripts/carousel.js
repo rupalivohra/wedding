ede66cc6f0e84b039c7f8849c238d54831763889
Rupali Vohra <rupalivohra@users.noreply.github.com>
2025-03-14 00:10:54 -0700

    Convert the events to a carousel (#22)

---
+++ scripts/carousel.js
@@ -0,0 +1,30 @@
+let eventIndex = 1;
+showEvents(eventIndex);
+
+// Next/previous controls
+function plusEvents(n) {
+    showEvents(eventIndex += n);
+}
+
+// Thumbnail image controls
+function currentEvent(n) {
+    showEvents(eventIndex = n);
+}
+
+function showEvents(n) {
+    let i;
+    let events = document.getElementsByClassName("events");
+    let dots = document.getElementsByClassName("dot");
+    if (n > events.length) { eventIndex = 1 }
+    if (n < 1) { eventIndex = events.length }
+    for (i = 0; i < events.length; i++) {
+        events[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    events[eventIndex - 1].style.display = "block";
+    if (dots.length > 0) {
+        dots[eventIndex - 1].className += " active";
+    }
+}
