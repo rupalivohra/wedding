429c46d41988c8721c47e908e6177d73ef57a48a
Rupali Vohra <rupalivohra@users.noreply.github.com>
2025-04-13 13:39:19 -0700

    Add gtag events for various interactions (#34)

---
+++ scripts/analytics.js
@@ -0,0 +1,71 @@
+function clickOnRsvpButton() {
+    gtag('event', 'Click on RSVP button', {});
+}
+
+function clickOnRsvpLink() {
+    gtag('event', 'Click on RSVP link', {});
+}
+
+function clickOnNavbarHamburger() {
+    gtag('event', 'Click on Hamburger menu (Mobile Only)', {});
+}
+
+function clickOnNavWelcome() {
+    gtag('event', 'Click on Welcome from Navbar', {});
+}
+
+function clickOnNavEvents() {
+    gtag('event', 'Click on Events from Navbar', {});
+}
+
+function clickOnNavRSVP() {
+    gtag('event', 'Click on RSVP section from Navbar', {});
+}
+
+function clickOnNavOurStory() {
+    gtag('event', 'Click on Our Story from Navbar', {});
+}
+
+function clickOnNavTravel() {
+    gtag('event', 'Click on Travel from Navbar', {});
+}
+
+function clickOnNavAccommodation() {
+    gtag('event', 'Click on Accommodation from Navbar', {});
+}
+
+function clickOnNavRegistry() {
+    gtag('event', 'Click on Registry from Navbar', {});
+}
+
+function clickOnNavFaq() {
+    gtag('event', 'Click on FAQ from Navbar', {});
+}
+
+function clickOnPreviousEventArrow() {
+    gtag('event', 'Click on "Previous Event" arrow', {});
+}
+
+function clickOnNextEventArrow() {
+    gtag('event', 'Click on "Next Event" arrow', {});
+}
+
+function clickOnWelcomeEventDot() {
+    gtag('event', 'Click on Welcome Party event dot', {});
+}
+
+function clickOnWeddingEventDot() {
+    gtag('event', 'Click on Wedding event dot', {});
+}
+
+function clickOnBrunchEventDot() {
+    gtag('event', 'Click on Brunch event dot', {});
+}
+
+function clickOnMariottHotelBlockLink() {
+    gtag('event', 'mariott_hotel_block', {});
+}
+
+function clickOnAlexisHotelBlockLink() {
+    gtag('event', 'alexis_hotel_block', {});
+}
