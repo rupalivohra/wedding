062714d234152b155fada7438274de4991a348b5
Rupali Vohra <rupali2vohra@gmail.com>
2025-04-13 13:15:54 -0700

    Add gtag events for various interactions

---
+++ scripts/analytics.js
@@ -0,0 +1,71 @@
+function clickOnRsvpButton() {
+    gtag('event', 'rsvp_view_button', {});
+}
+
+function clickOnRsvpLink() {
+    gtag('event', 'rsvp_view_link', {});
+}
+
+function clickOnNavbarHamburger() {
+    gtag('event', 'mobile_navbar_view', {});
+}
+
+function clickOnNavWelcome() {
+    gtag('event', 'nav_welcome', {});
+}
+
+function clickOnNavEvents() {
+    gtag('event', 'nav_events', {});
+}
+
+function clickOnNavRSVP() {
+    gtag('event', 'nav_rsvp', {});
+}
+
+function clickOnNavOurStory() {
+    gtag('event', 'nav_our_story', {});
+}
+
+function clickOnNavTravel() {
+    gtag('event', 'nav_travel', {});
+}
+
+function clickOnNavAccommodation() {
+    gtag('event', 'nav_accommodation', {});
+}
+
+function clickOnNavRegistry() {
+    gtag('event', 'nav_registry', {});
+}
+
+function clickOnNavFaq() {
+    gtag('event', 'nav_faq', {});
+}
+
+function clickOnPreviousEventArrow() {
+    gtag('event', 'prev_event_arrow', {});
+}
+
+function clickOnNextEventArrow() {
+    gtag('event', 'next_event_arrow', {});
+}
+
+function clickOnWelcomeEventDot() {
+    gtag('event', 'welcome_event_dot', {});
+}
+
+function clickOnWeddingEventDot() {
+    gtag('event', 'wedding_event_dot', {});
+}
+
+function clickOnBrunchEventDot() {
+    gtag('event', 'brunch_event_dot', {});
+}
+
+function clickOnMariottHotelBlockLink() {
+    gtag('event', 'mariott_hotel_block', {});
+}
+
+function clickOnAlexisHotelBlockLink() {
+    gtag('event', 'alexis_hotel_block', {});
+}
